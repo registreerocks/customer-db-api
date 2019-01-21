b2f3d4f012418bbcdad44ac2bbba028cf90e336c
Sabine <sabine.bertram@mailbox.org>
2019-01-20 18:39:27 -0800

    feat: add getter functions, refactor: rename collections

--- package/src/swagger_server/controllers/customer_db_functions.py
+++ package/src/swagger_server/controllers/customer_db_functions.py
@@ -6,8 +6,8 @@ from .authentication import requires_auth, requires_scope
 client = MongoClient('mongodb://mongodb:27017/')
 db = client.customer_database
 customer_details = db.customer_details
-invoice_details = db.invoice_details
-payment_status = db.payment_status
+payment_details = db.payment_details
+invoices = db.invoices
 
 @requires_auth
 @requires_scope('registree')
@@ -40,42 +40,23 @@ def put_customer(id, body):
 
 @requires_auth
 @requires_scope('registree')
-def post_invoice(body):
-    return str(invoice_details.insert_one(body).inserted_id)
-
-@requires_auth
-@requires_scope('registree', 'recruiter')
-def get_invoice(id):
-    result = invoice_details.find_one({'_id': ObjectId(id)})
-    if result:
-        result['_id'] = str(result['_id'])
-        return result
-    else:
-        return {'ERROR': 'No matching data found.'}, 409
-
-@requires_auth
-@requires_scope('registree')
-def put_invoice(id, body):
-    result = invoice_details.find_one_and_update(
-                {'_id': ObjectId(id)},
-                {'$set':{body.get('field'): body.get('value')}},
-                return_document=ReturnDocument.AFTER
-            )
-    if result:
-        result['_id'] = str(result['_id'])
-        return result
-    else:
-        return {'ERROR': 'No matching data found.'}, 409
-
-@requires_auth
-@requires_scope('registree')
 def post_payment(body):
-    return str(payment_status.insert_one(body).inserted_id)
+    return str(payment_details.insert_one(body).inserted_id)
 
 @requires_auth
 @requires_scope('registree', 'recruiter')
 def get_payment(id):
-    result = payment_status.find_one({'_id': ObjectId(id)})
+    result = payment_details.find_one({'_id': ObjectId(id)})
+    if result:
+        result['_id'] = str(result['_id'])
+        return result
+    else:
+        return {'ERROR': 'No matching data found.'}, 409
+
+@requires_auth
+@requires_scope('registree', 'recruiter')
+def get_payment_by_customer(customer_id):
+    result = payment_details.find_one({'customer_id': customer_id})
     if result:
         result['_id'] = str(result['_id'])
         return result
@@ -85,7 +66,7 @@ def get_payment(id):
 @requires_auth
 @requires_scope('registree')
 def put_payment(id, body):
-    result = payment_status.find_one_and_update(
+    result = payment_details.find_one_and_update(
                 {'_id': ObjectId(id)},
                 {'$set':{body.get('field'): body.get('value')}},
                 return_document=ReturnDocument.AFTER
@@ -95,3 +76,76 @@ def put_payment(id, body):
         return result
     else:
         return {'ERROR': 'No matching data found.'}, 409
+
+@requires_auth
+@requires_scope('registree')
+def post_invoice(body):
+    return str(invoices.insert_one(body).inserted_id)
+
+@requires_auth
+@requires_scope('registree', 'recruiter')
+def get_invoice(id):
+    result = invoices.find_one({'_id': ObjectId(id)})
+    if result:
+        result['_id'] = str(result['_id'])
+        return result
+    else:
+        return {'ERROR': 'No matching data found.'}, 409
+
+@requires_auth
+@requires_scope('registree', 'recruiter')
+def get_invoice_by_query(query_id):
+    result = invoices.find_one({'query_id': query_id})
+    if result:
+        result['_id'] = str(result['_id'])
+        return result
+    else:
+        return {'ERROR': 'No matching data found.'}, 409
+
+@requires_auth
+@requires_scope('registree', 'recruiter')
+def get_invoices_by_customer(customer_id):
+    result = invoices.find({'customer_id': customer_id})
+    if result:
+        return _stringify_object_id(result)
+    else:
+        return {'ERROR': 'No matching data found.'}, 409
+
+@requires_auth
+@requires_scope('registree', 'recruiter')
+def get_invoices_by_customer_paid(customer_id):
+    result = invoices.find({'customer_id': customer_id, 'payment.complete': 'true'})
+    if result:
+        return _stringify_object_id(result)
+    else:
+        return {'ERROR': 'No matching data found.'}, 409
+    
+@requires_auth
+@requires_scope('registree', 'recruiter')
+def get_invoices_by_customer_open(customer_id):
+    result = invoices.find({'customer_id': customer_id, 'payment.complete': 'false'})
+    if result:
+        return _stringify_object_id(result)
+    else:
+        return {'ERROR': 'No matching data found.'}, 409
+
+@requires_auth
+@requires_scope('registree')
+def put_invoice(id, body):
+    result = invoices.find_one_and_update(
+                {'_id': ObjectId(id)},
+                {'$set':{body.get('field'): body.get('value')}},
+                return_document=ReturnDocument.AFTER
+            )
+    if result:
+        result['_id'] = str(result['_id'])
+        return result
+    else:
+        return {'ERROR': 'No matching data found.'}, 409
+
+def _stringify_object_id(result):
+    stringified_result = []
+    for element in result:
+        element['_id'] = str(element['_id'])
+        stringified_result.append(element)
+    return stringified_result
